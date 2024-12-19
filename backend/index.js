b471c3bfbcd1d728728cde95870ddb98f2b4d019
fouuuadi <fouadlamnaouar18@gmail.com>
2024-12-19 14:19:27 +0100

    feat: add various files

--- backend/index.js
+++ backend/index.js
@@ -10,16 +10,39 @@ dotenv.config();
 
 const app = express();
 
+
 // Middleware
 app.use(bodyParser.json());
+app.use(express.json());
 
 // Routes
 app.use('/api', taskRoutes);
 
 // le lien de route http://localhost:3000/
+// app.get('/', (req, res) => {
+//     res.send('La route marche')
+// });
+
 app.get('/', (req, res) => {
-    res.send('La route marche')
+    const query = 'SELECT * FROM tasks';
+
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Erreur lors de la récupération des tâches :', err.message);
+            res.status(500).send('Erreur lors de la récupération des données.');
+            return;
+        }
+
+        // Affiche les données dans la réponse
+        res.send(`
+            <h1>Liste des Tâches</h1>
+            <ul>
+                ${results.map(task => `<li><strong>${task.nameTask}</strong>: ${task.descriptionTask}</li>`).join('')}
+            </ul>
+        `);
+    });
 });
+
 
 // Démarrer le serveur
 const PORT = process.env.PORT || 3000;
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -11,15 +11,17 @@ router.get('/', (req, res) => {
 
 export default router; // Exporter le router pour l'utiliser dans index.js
 
-// router.get('/', (req, res) => {
-//     const query = 'SELECT * FROM DevOps';
-//     db.query(query, (err, results) => {
-//         if (err) {
-//             console.error('Erreur lors de la récupération des utilisateurs :', err.message);
-//             return res.status(500).json({ error: 'Erreur serveur' });
-//         }
-//         res.status(200).json(results);
-//     });
-// });
+// Route pour récupérer toutes les tâches
+router.get('/tasks', (req, res) => {
+    const query = 'SELECT * FROM tasks';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Erreur lors de la récupération des tâches :', err.message);
+            return res.status(500).json({ error: 'Erreur serveur' });
+        }
+        res.status(200).json(results);
+    });
+});
+
 
 // export default router;
